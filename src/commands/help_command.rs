5937613cc875f576cd9e624787460f4f1aa8ffb6
Sep <septechx@proton.me>
2025-07-25 03:50:27 +0200

    feat: Add command listing to help command

--- src/commands/help_command.rs
+++ src/commands/help_command.rs
@@ -1,6 +1,6 @@
 use serenity::{async_trait, model::channel::Message, prelude::*};
 
-use super::{args_checked, send_error, CommandHandler};
+use super::{args, send_error, usage, CommandHandler};
 
 use crate::Handler;
 
@@ -15,25 +15,44 @@ impl HelpHandler {
 #[async_trait]
 impl CommandHandler for HelpHandler {
     fn get_usage(&self) -> &'static str {
-        "$help command"
+        "$help (command)"
     }
 
     async fn handle(&self, state: &Handler, ctx: &Context, msg: &Message) -> serenity::Result<()> {
         let usage_s = self.get_usage();
-        let args = match args_checked(ctx, msg, 1, usage_s).await {
-            Ok(a) => a,
-            Err(_) => return Ok(()),
-        };
+        let args = args(msg);
 
-        let cmd = args[0];
-        let help = match state.command_handlers.handlers.get(cmd) {
-            Some(ch) => ch.get_usage(),
-            None => {
-                send_error(ctx, msg, &format!("Command not found: {cmd}")).await?;
-                return Ok(());
+        match args.len() {
+            0 => {
+                let cmds = state
+                    .command_handlers
+                    .handlers
+                    .values()
+                    .map(|ch| ch.get_usage())
+                    .collect::<Vec<_>>()
+                    .join("\n");
+
+                msg.channel_id
+                    .say(&ctx.http, format!("Commands:\n{cmds}"))
+                    .await?;
             }
-        };
+            1 => {
+                let cmd = args[0];
+                let help = match state.command_handlers.handlers.get(cmd) {
+                    Some(ch) => ch.get_usage(),
+                    None => {
+                        send_error(ctx, msg, &format!("Command not found: {cmd}")).await?;
+                        return Ok(());
+                    }
+                };
 
-        msg.channel_id.say(&ctx.http, help).await.map(|_| ())
+                msg.channel_id.say(&ctx.http, help).await?;
+            }
+            2.. => {
+                msg.channel_id.say(&ctx.http, usage(usage_s)).await?;
+            }
+        }
+
+        Ok(())
     }
 }
